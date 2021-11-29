ecfaf2f5e66caa03513a2efdc6016009fad6cd26
Meidani Pamungkas <68596953+meydanipamungkas@users.noreply.github.com>
2021-11-29 15:27:22 +0700

    Update plot1.R

--- Project1/plot1.R
+++ Project1/plot1.R
@@ -1,5 +1,5 @@
 #data preprocessing
-library(data.table)
+library(read.table)
 power<-read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 power2<-subset(power,power$Date=="1/2/2007"|power$Date=="2/2/2007")
